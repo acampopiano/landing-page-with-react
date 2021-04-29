936c6d4e469c3eeb138d61a7ece84effdda3228a
Andres Campopiano <acampopiano@gmail.com>
2021-04-29 14:13:43 +0000

    subo proyecto con json file

--- src/js/component/cards2.jsx
+++ src/js/component/cards2.jsx
@@ -7,32 +7,44 @@ export const Cards2 = props => {
 	};
 
 	return (
-		<div className="col-lg-3 col-md-12 mx-auto">
-			<div className="card card-block" style={styles}>
-				<div className="card-header">
-					<img
-						src={props.imageUrl}
-						className="card-img-top img-fluid"
-						alt="Imagen"></img>
-				</div>
-				<div className="card-body">
-					<h5 className="card-title">{props.name}</h5>
-					<p className="card-text">{props.description}</p>
-				</div>
-				<div className="card-footer">
-					<a href={props.buttonUrl} className="btn btn-primary">
-						{props.buttonLabel}
-					</a>
-				</div>
-			</div>
+		<div className="row mt-3 mb-3">
+			{props.data
+				? props.data.map((item, i) => (
+						<div
+							key={`${i}`}
+							className="col-lg-3 col-md-12 mx-auto">
+							<div className="card card-block" style={styles}>
+								<div className="card-header">
+									<img
+										src={item.imageURL}
+										className="card-img-top img-fluid"
+										alt="Imagen"></img>
+								</div>
+								<div className="card-body">
+									<h5 className="card-title">{item.name}</h5>
+									<p className="card-text">
+										{item.description}
+									</p>
+								</div>
+								<div className="card-footer">
+									<a
+										href={item.buttonURL}
+										className="btn btn-primary">
+										{item.buttonLabel}
+									</a>
+								</div>
+							</div>
+						</div>
+				  ))
+				: "Loading..."}
 		</div>
 	);
 };
 
 Cards2.propTypes = {
-	title: PropTypes.string,
+	name: PropTypes.string,
 	description: PropTypes.string,
-	imageUrl: PropTypes.string,
+	imageURL: PropTypes.string,
 	buttonLabel: PropTypes.string,
-	buttonUrl: PropTypes.string
+	buttonURL: PropTypes.string
 };
